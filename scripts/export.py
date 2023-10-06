479766069030b0594bdd36da40b219d856607e23
Cameron Lee <cameron@solomonpierce.com>
2023-10-06 03:54:21 +0000

    Final tweaks to the scripts

--- scripts/export.py
+++ scripts/export.py
@@ -3,15 +3,16 @@ import ebooklib
 from ebooklib import epub
 from markdown2 import markdown
 
-# another one from chad gpt
-# i havent even tried running it
-# honestly it may be eaiser to just use Calibre 
+# I had Chat-GPT spit this out. It's a good start. It
+# includes two options for creating a .mobi file, I opted
+# for the Calibre option. In the end that may be unnecessary
+# since Amazon is apparently using the .epub format now.
 
 
-# Ensure the 'dist' directory exists
 os.makedirs('../dist', exist_ok=True)
 
 def convert_md_to_html(md_file_path):
+    print(md_file_path)
     with open(md_file_path, 'r', encoding='utf-8') as md_file:
         md_content = md_file.read()
         html_content = markdown(md_content)
@@ -42,7 +43,7 @@ def create_mobi_kindlegen(epub_file_path, mobi_file_path):
     os.system(command)
 
 def main():
-    md_file_path = '../tokenomics-book.md'
+    md_file_path = '/workspace/tokenomics-book.md'
     epub_file_path = '../dist/tokenomics-book.epub'
     mobi_file_path = '../dist/tokenomics-book.mobi'
 
@@ -50,8 +51,8 @@ def main():
     create_epub(html_content, epub_file_path)
 
     # Choose one of the following two lines based on the conversion tool you have installed:
-    # create_mobi_calibre(epub_file_path, mobi_file_path)  # Requires Calibre
-    create_mobi_kindlegen(epub_file_path, mobi_file_path)  # Requires KindleGen
+    create_mobi_calibre(epub_file_path, mobi_file_path)  # Requires Calibre
+    # create_mobi_kindlegen(epub_file_path, mobi_file_path)  # Requires KindleGen
 
 if __name__ == '__main__':
     main()
